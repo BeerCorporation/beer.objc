8530b9efff5c4e6d4d1214a926d8825d0e76bbc0
Nanouk <nanouk@nxtelevision.com>
2015-03-29 23:31:09 +0200

    Create Xcode project

---
+++ Beer/Beer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Beer
+//
+//  Created by Jean-Baptiste Bayle on 3/29/15.
+//  Copyright (c) 2015 Beer Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
